7415c6487b9153a953525cfad166a6abd16253f3
saforer <saforer@gmail.com>
2016-03-27 10:11:43 -0400

    Create Tile.java
    Coding ahead of time for libgdx stuffs

---
+++ Tile.java
@@ -0,0 +1,34 @@
+public class Tile {
+  private int gridX;
+  private int gridY;
+  private Texture icon;
+  private State tileState;
+  
+  //Static textures to load
+  private static Texture normalTile;
+  
+  public Tile (int x, int y) {
+    gridX = x;
+    gridY = y;
+    if (normal == null) {
+      loadStatic();
+    }
+    updateTile(); 
+  }
+  
+  void loadStatic() {
+    normal = new Texture("normalTile.png");
+  }
+  
+  void updateTile() {
+    switch (tileState) {
+      default:
+        icon = normalTile;
+    }
+  }
+  
+  public void draw(SpriteBatch sb) {
+    int tileSize = 32;
+    sb.draw(icon, gridX * tileSize, gridY * tileSize);
+  }
+}
