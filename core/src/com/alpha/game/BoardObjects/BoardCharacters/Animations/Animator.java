646eeb858a7739b265e13eb455a28a74ba2c4256
saforer <saforer@gmail.com>
2016-04-03 07:39:15 -0400

    Create Animator.java

---
+++ core/src/com/alpha/game/BoardObjects/BoardCharacters/Animations/Animator.java
@@ -0,0 +1,18 @@
+//This is just to keep track of the animations that a particular thing has
+package com.alpha.game.BoardObjects.BoardCharacters.Animations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public cass Animator {
+  Map<String, Animation> animationDictionary = new HashMap<String, Animation>();
+  Animation currentAnimation;
+  
+  public Animator () {
+    
+  }
+  
+  public void update (float dt) {
+    
+  }
+}
